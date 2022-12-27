ef80731ab7ba2facec80b7a04d4e33dc9468476c
Mariia Kovalova <mashuha1698@gmail.com>
2022-12-27 13:03:52 +0200

    Updates

--- src/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
-// https://uk.reactjs.org/docs/components-and-props.html
-// https://github.com/Mariliova/logoipsum
